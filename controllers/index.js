d65d81980847c93a39baca0ae371036b92bb7139
Alan Smith <alan.smith@livingsocial.com>
2015-04-29 10:13:29 -0600

    defaults to use ip address for geolocation if no params are given

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,21 @@
 var getWeather = require('../services/getWeather');
 var Controller = {};
 var Async = require('async');
+var geoip = require('geoip-lite');
 
 Controller.index = function(req, response) {
-  var params = req.query;
+  var params = getParams();
+
+  function getParams() {
+    if(req.query.lat !== undefined && req.query.lon !== undefined) {
+      return req.query;
+    } else {
+        var ip  = '67.177.208.157'; // just a static IP for now
+        var geo = geoip.lookup(ip)['ll'];
+        
+        return { 'lat': geo[0], 'lon': geo[1] };
+      }
+  }
 
   Async.series([
     function(callback) {
